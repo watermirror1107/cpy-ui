32c8ed740e172687a1eda6a1e437ee940c8a2bd2
gaoc <2286949954@qq.com>
2022-07-28 17:13:22 +0800

    feat:更改图标库

--- packages/index.js
+++ packages/index.js
@@ -1,7 +1,7 @@
 //先导入UI字体库
 import './font/font_2142578_45182ujwnjc.js'//等UI补全图标后删除
 import './font/font_2631791_umj4vo54h.js'//等UI补全图标后删除
-import './font/font_3379171_y950vki2zz.js'//new
+import './font/font_ecp_3.0.js'//new
 //导入第三方UI库
 import '../ant-design-vue/index'
 import CModal from "./CModal/index.js";
