807047fb29fd5e919de756fc4b05ae87c74ef460
songjie55 <ooo88888@live.cn>
2022-10-18 14:40:45 +0800

    feat:修改vue版本

--- packages/index.js
+++ packages/index.js
@@ -1,14 +1,14 @@
 //先导入UI字体库
-import './font/font_3379171_aznauuqpx0g.js'//对应iconfont项目里面的ECP-3.0 
+import './font/font_3379171_aznauuqpx0g.js'//对应iconfont项目里面的ECP-3.0
 import './font/font_2142578_45182ujwnjc' //对应iconfont项目ECP-portal
-import './font/font_2631791_umj4vo54h' //未知  
+import './font/font_2631791_umj4vo54h' //未知
 //以旧的覆盖新的，新的名字一般不一样，不会被覆盖
 //导入第三方UI库
 import '../ant-design-vue/index'
- 
+
 const components = []
 //导入组件
-const req = require.context('./', true, /\.vue$/); 
+const req = require.context('./', true, /\.vue$/);
 req.keys().forEach((item) => {
     const exports = req(item).default;
     components.push(exports);
@@ -23,9 +23,9 @@ function install(Vue) {
 
 if (typeof window !== 'undefined' && window.Vue) {
     install(window.Vue)
-} 
- 
+}
+
 export default {
     install,
-    version: '0.2.24'
+    version: '0.2.25'
 }
