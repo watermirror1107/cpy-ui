4f038166fb946e8d33a4d71560be86a241a0037c
songjie55 <ooo88888@live.cn>
2022-12-02 15:55:02 +0800

    feat:更新admin端的图标库

--- packages/index.js
+++ packages/index.js
@@ -1,6 +1,6 @@
 //先导入UI字体库
 import './font/font_3379171_aznauuqpx0g.js'//对应iconfont项目里面的ECP-3.0
-import './font/font_2142578_45182ujwnjc' //对应iconfont项目ECP-portal
+import './font/font_2142578_oihzpgeg849' //对应iconfont项目ECP-portal
 import './font/font_2631791_umj4vo54h' //未知
 //以旧的覆盖新的，新的名字一般不一样，不会被覆盖
 //导入第三方UI库
@@ -27,5 +27,5 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 export default {
     install,
-    version: '0.2.27'
+    version: '0.2.28'
 }
